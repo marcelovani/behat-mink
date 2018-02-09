d2abd21c67130755df9d97102aa688c22758a763
Juan Colomer <juan_colomer@dennis.co.uk>
2018-02-09 17:40:52 +0000

    Case 28892; Add HTML context.

---
+++ src/Context/HtmlContext.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace DennisDigital\Behat\Mink\Html\Context;
+
+use Behat\Behat\Context\Context;
+
+use Behat\Mink\Mink;
+use Behat\MinkExtension\Context\MinkAwareContext;
+
+/**
+ * Class HtmlContext
+ * @package DennisDigital\Behat\Mink\Html\Context
+ */
+class HtmlContext implements MinkAwareContext {
+
+}
